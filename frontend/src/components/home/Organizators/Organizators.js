4eace3c5edd1aa6c3e268e0f47da9106dbdb7920
mariem hacheni <hachanimariem16@gmail.com>
2021-02-22 17:17:33 +0100

    responsive team component

--- frontend/src/components/home/Organizators/Organizators.js
+++ frontend/src/components/home/Organizators/Organizators.js
@@ -9,34 +9,39 @@ const Organizators = () => {
                 <h2 className="display-4">Organization team</h2>
             </Container>
             <Container>
-                <div className="organizator__content">
-                    <div className="organizator__intro order-1">
-                        uifgzefombfi
-                    </div>
-                    <div className="organizator__image order-2" style={{
+                <Row className="organizator__content">
+
+                    <Col className="organizator__image order-2" style={{
                         backgroundImage: 'url("./images/119511418_3452294868161567_2266724771039006581_o.jpg")'
                     }}>
-                    </div>
-                </div>
-            </Container>
-            <Container>
-                <Row className="organizator__content">
-                    <Col className="organizator__intro order-2">
+
+                    </Col><Col className="organizator__intro order-1">
                         uifgzefombfi
                     </Col>
-                    <Col className="organizator__image order-1">
-                        fhruoforbgkl
-                </Col>
                 </Row>
             </Container>
             <Container>
                 <Row className="organizator__content">
-                    <Col className="organizator__intro order-1">
+
+                    <Col className="organizator__image order-1" style={{
+                        backgroundImage: 'url("./images/119511418_3452294868161567_2266724771039006581_o.jpg")'
+                    }}>
+
+                    </Col><Col className="organizator__intro order-2">
                         uifgzefombfi
                     </Col>
-                    <Col className="organizator__image order-2">
-                        fhruoforbgkl
-                </Col>
+                </Row>
+            </Container>
+            <Container>
+                <Row className="organizator__content">
+
+                    <Col className="organizator__image order-2" style={{
+                        backgroundImage: 'url("./images/119511418_3452294868161567_2266724771039006581_o.jpg")'
+                    }}>
+
+                    </Col> <Col className="organizator__intro order-1">
+                        uifgzefombfi
+                    </Col>
                 </Row>
             </Container>
         </Jumbotron>
